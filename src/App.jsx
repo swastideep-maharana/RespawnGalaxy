ed5549dc3503c8e94817414419119828844b199b
swastideep maharana <swastideep67maharana@gmail.com>
2024-11-25 06:18:08 +0530

    #added the hero section and videos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <main>
-      <h1 className="text-5xl font-bold text-violet-300 ">Welcome to the website</h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
     </main>
   );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,109 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import Button from "./Button";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [loadedVideos, setLoadedVideos] = useState(0);
+  const [loading, setLoading] = useState(true);
 
-export default Hero
+  const totalVideos = 3;
+  const nextVdRef = useRef(null);
+
+  const handleVideoLoad = () => {
+    setLoadedVideos((prev) => prev + 1);
+    if (loadedVideos + 1 >= totalVideos) {
+      setLoading(false); // Hide the loader when all videos are loaded
+    }
+  };
+
+  const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+
+  const handleMiniVdClick = () => {
+    setCurrentIndex(upcomingVideoIndex);
+  };
+
+  const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
+
+  return (
+    <div className="relative h-dvh w-screen overflow-x-hidden">
+      {/* Loader */}
+      {loading && (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          {/* https://uiverse.io/G4b413l/tidy-walrus-92 */}
+          <div className="three-body">
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+          </div>
+        </div>
+      )}
+
+      {/* Video frame */}
+      <div
+        id="video-frame"
+        className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
+      >
+        <div>
+          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+            <div
+              onClick={handleMiniVdClick}
+              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
+            >
+              <video
+                ref={nextVdRef}
+                src={getVideoSrc(upcomingVideoIndex)}
+                loop
+                muted
+                id="current-video"
+                className="size-64 origin-center scale-150 object-cover object-center"
+                onLoadedData={handleVideoLoad}
+              />
+            </div>
+          </div>
+
+          {/* Preload next video */}
+          <video
+            ref={nextVdRef}
+            src={getVideoSrc(currentIndex)}
+            loop
+            muted
+            id="next-video"
+            className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+
+          {/* Autoplay current video */}
+          <video
+            src={getVideoSrc(currentIndex)}
+            // autoPlay
+            loop
+            muted
+            className="absolute left-0 top-0 size-full object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+        </div>
+
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+          G<b className="text-violet-300">a</b>ming
+        </h1>
+        {/* <b className="text-yellow-300">R</b>espawing <b className="text-violet-300">G</b>alaxy */}
+        <div className="absolute left-0 top-0 z-40 size-full ">
+          <div className="mt-24 px-5 sm:px-10">
+            <h1 className="special-font hero-heading text-blue-100">
+              redefi<b className="text-violet-300">n</b>e
+            </h1>
+
+            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+              Enter the Metagame Layer <br />
+              Unleash the Play Economy
+            </p>
+
+            <Button id="watch-trailer" title="Watch Trailer" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
