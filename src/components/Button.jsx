fc76e391e118bc7c7ba9ee5e6775ce7c0cd37185
swastideep maharana <swastideep67maharana@gmail.com>
2024-11-25 07:38:28 +0530

    #added the gsap and animation

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-const Button = () => {
+const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
   return (
-    <div>Button</div>
-  )
-}
+    <button
+      id={id}
+      className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}
+    >
+      {leftIcon}
 
-export default Button
+      <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
+        <div>{title}</div>
+      </span>
+
+      {rightIcon}
+    </button>
+  );
+};
+
+export default Button;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,18 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 
+gsap.registerPlugin(ScrollTrigger);
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
   const [loadedVideos, setLoadedVideos] = useState(0);
   const [loading, setLoading] = useState(true);
 
-  const totalVideos = 3;
+  const totalVideos = 4;
   const nextVdRef = useRef(null);
 
   const handleVideoLoad = () => {
@@ -21,6 +27,57 @@ const Hero = () => {
   const handleMiniVdClick = () => {
     setCurrentIndex(upcomingVideoIndex);
   };
+
+  useEffect(() => {
+    if (loadedVideos === totalVideos - 1) {
+      setLoading(false);
+    }
+  }, [loadedVideos]);
+
+  useGSAP(
+    () => {
+      if (hasClicked) {
+        gsap.set("#next-video", { visibility: "visible" });
+        gsap.to("#next-video", {
+          transformOrigin: "center center",
+          scale: 1,
+          width: "100%",
+          height: "100%",
+          duration: 1,
+          ease: "power1.inOut",
+          onStart: () => nextVdRef.current.play(),
+        });
+        gsap.from("#current-video", {
+          transformOrigin: "center center",
+          scale: 0,
+          duration: 1.5,
+          ease: "power1.inOut",
+        });
+      }
+    },
+    {
+      dependencies: [currentIndex],
+      revertOnUpdate: true,
+    }
+  );
+
+  useGSAP(() => {
+    gsap.set("#video-frame", {
+      clipPath: "polygon(14% 0, 72% 0, 88% 90%, 0 95%)",
+      borderRadius: "0% 0% 40% 10%",
+    });
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      borderRadius: "0% 0% 0% 0%",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
+  });
 
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
@@ -75,7 +132,7 @@ const Hero = () => {
           {/* Autoplay current video */}
           <video
             src={getVideoSrc(currentIndex)}
-            // autoPlay
+            autoPlay
             loop
             muted
             className="absolute left-0 top-0 size-full object-cover object-center"
@@ -84,24 +141,34 @@ const Hero = () => {
         </div>
 
         <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
-          G<b className="text-violet-300">a</b>ming
+          {/* G<b className="text-violet-300">a</b>ming */}
+          <b className="text-yellow-300">R</b>espawing{" "}
+          <b className="text-violet-300">G</b>alaxy
         </h1>
-        {/* <b className="text-yellow-300">R</b>espawing <b className="text-violet-300">G</b>alaxy */}
+
         <div className="absolute left-0 top-0 z-40 size-full ">
           <div className="mt-24 px-5 sm:px-10">
             <h1 className="special-font hero-heading text-blue-100">
               redefi<b className="text-violet-300">n</b>e
             </h1>
-
             <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
-              Enter the Metagame Layer <br />
-              Unleash the Play Economy
+              Enter the Metagame Layer <br /> Unleash the Play Economy
             </p>
 
-            <Button id="watch-trailer" title="Watch Trailer" />
+            <Button
+              id="watch-trailer"
+              title="Watch Trailer"
+              leftIcon={<TiLocationArrow />}
+              containerClass="!bg-yellow-300 flex-center gap-1"
+            />
           </div>
         </div>
       </div>
+
+      <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
+        <b className="text-yellow-300">R</b>espawing{" "}
+        <b className="text-violet-300">G</b>alaxy
+      </h1>
     </div>
   );
 };
