78373573deeffda2460595f95b81f7120993a248
swastideep maharana <swastideep67maharana@gmail.com>
2024-11-26 08:36:01 +0530

    #added About page

---
+++ src/components/About.jsx
@@ -0,0 +1,57 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import React from "react";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+const About = () => {
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#clip",
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
+      borderRadius: 0,
+    });
+  });
+  return (
+    <div id="about" className="min-h-screen w-screen">
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <h2 className="font-general text-sm uppercase md:text-[30px]">
+          Welcome to Zentry
+        </h2>
+
+        <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+          Disc<b className="text-violet-500">O</b>ver the World's <br /> l
+          <b className="text-violet-500">a</b>rgest shared Adventure,
+        </div>
+
+        <div className="about-subtext">
+          <p>The Game of Games begins-your life, now an epic MMORPG</p>
+          <p>Zentry unites every player from countless games and platforms.</p>
+        </div>
+      </div>
+
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/about.webp"
+            alt="Background"
+            className="absolute top-0 left-0 size-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -86,7 +86,6 @@ const Hero = () => {
       {/* Loader */}
       {loading && (
         <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
-          {/* https://uiverse.io/G4b413l/tidy-walrus-92 */}
           <div className="three-body">
             <div className="three-body__dot"></div>
             <div className="three-body__dot"></div>
