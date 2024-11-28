7a0648eada8290b9425da6773a1de4175870cfcb
swastideep maharana <swastideep67maharana@gmail.com>
2024-11-28 15:19:27 +0530

    #added the features section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -131,7 +131,7 @@ const Hero = () => {
           {/* Autoplay current video */}
           <video
             src={getVideoSrc(currentIndex)}
-            // autoPlay
+            autoPlay
             loop
             muted
             className="absolute left-0 top-0 size-full object-cover object-center"
