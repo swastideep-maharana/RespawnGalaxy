d16fb5d1fb8295a35bea5894fd2291aa2717c43e
swastideep maharana <swastideep67maharana@gmail.com>
2024-11-28 15:22:35 +0530

    #add the texts

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Story = () => {
   return (
-    <div>Story</div>
-  )
-}
+    <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+      <div className="flex size-full flex-col items-center py-10 pb-24">
+        <p>The multiversal ip World</p>
+      </div>
+    </section>
+  );
+};
 
-export default Story
+export default Story;
